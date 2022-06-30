93ee40f4ea53146ab57ff090b6df91a03484d9eb
Kotall1 <kvas.ahabanin@gmail.com>
2022-06-30 22:44:49 +0300

    Modified post php

--- web/post.php
+++ web/post.php
@@ -1,9 +1,11 @@
 <?php
-session_start();
+declare(strict_types=1);
+
 include("form.html");
+session_start();
+
 $_SESSION["name"] = "Sam";
 $_SESSION["age"] = 27;
 echo "Данные в сессию сохранены";
 
 
-var_dump($_SESSION);
